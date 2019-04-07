87158f6c352536db0e89a0f19abcedd9e676a7d7
Jimmy Stone <jstone05@hamline.edu>
2019-04-07 14:59:56 -0500

    notes

--- public/assets/app.js
+++ public/assets/app.js
@@ -60,65 +60,45 @@ $(".delete-article").click(function() {
 })
 
 //routes for note taking
-$("#noteButton").on("click", function(){
-
-        // the article id
-        var ID = $(this).attr("data-value");
-  
-        //attach news article _id to the save button in the modal for use in save post
-        $("#modalNoteButton").attr({"data-value": ID});
-  
-        //make an ajax call for the notes attached to this article
-        $.get("/articles/" + ID, function(data){
-            
-
-            //empty modal title, textarea and notes
-            $('#noteModalLabel').empty();
-            $('#notesBody').empty();
-            $('#noteBody').val('');
-  
-            //delete button for individual note
-  
-            //add id of the current article to modal label
-            $('#noteModalLabel').append(' ' + ID);
-            //add notes to body of modal, will loop through if multiple notes
-            for(var i = 0; i<data.note.length; i++) {
-                console.log("loop for data", data.note[i]);
-                var button = ' <a href=/deleteNote/' + data.note[i]._id + '><i class="pull-right fa fa-times fa-2x deletex" aria-hidden="true"></i></a>';
-                $('#notesBody').append('<div class="panel panel-default"><div class="noteText panel-body">' + data.note[i].content + '  ' + button + '</div></div>');
-            }
-        });
-
-    });
-
 $(".savenote").on('click', function() {
-    var ID = $(this).attr("data-value"); 
-    console.log("This is my ID", ID );
+    var ID = $(this).attr("data-id"); 
+    // console.log("This is my ID", ID );
+  var input = {
+      body:$("#bodyinput").val(),
+      title: "Note"  
+    };
+//   console.log(input.body);
     $.ajax({
         method: "POST",
         url: "/articles/" + ID,
         data: {
-            body: $("#noteBody").val()
+            body:$("#bodyinput").val(),
+            title: "Note" 
         }
+
     })
-    .done(function(data) {
+    .then(function(data) {
+        console.log("This is my data from POST....",data)
         $('#noteModal').modal('hide');
     });
 });
 
-$("#noteButton").on('click', function() {
-    var ID = $(this).attr("data-value"); 
-    console.log("ID for the saved notes", ID);
+$(".note-button").on('click', function() {
+    var ID = $(this).attr("data-id"); 
+
+    // console.log("I'm in the route to log existing notes");
+
+    // console.log("ID for the saved notes", ID);
     $.ajax({
         method: "GET",
         url: "/articles/" + ID,
-        data: {
-            body: content
-        }
+
     })
-    .done(function(data) {
-        console.log("front-end notes to append",data);
-        $('#noteBody').append(data);
+    .then(function(data) {
+        // console.log("front-end notes to append",data);
+         if(data.note){
+            $("#bodyinput").val(data.note.body);
+         }
     })
 })
 
